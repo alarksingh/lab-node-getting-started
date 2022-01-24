795839d358628c85a2120038302c44121ce67958
Alark <alarksingh1999@gmail.com>
2022-01-24 19:44:06 +0530

    done

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 class SortedList {
-  constructor() {
-  
-  }
-  add() {
-   
-  }
-  get() {
+    constructor() {
+        this.items = []
+        this.length = 0
 
-  }
-  max() {
+    }
+    add(value) {
+        this.items.push(value)
+        this.items.sort((n1, n2) => n1 - n2);
+        this.length = this.items.length
+    }
+    get(index) {
+        if (index > this.length) throw new Error("OutOfBounds")
+        else return this.items.indexOf(index)
 
-  }
-  min() {
+    }
+    max() {
+        if (this.length == 0) throw new Error("EmptySortedList");
+        else return Math.max(...this.items)
 
-  }
-  avg() {
-    
-   
-  }
+    }
+    min() {
+        if (this.length == 0) throw new Error("EmptySortedList");
+        else return Math.min(...this.items)
 
-  sum() {
-  
-  }
-  
+
+    }
+    avg() {
+        if (this.length == 0) throw new Error("EmptySortedList");
+        else return (this.items.reduce((output, ele) => output + ele) / this.length)
+
+
+
+    }
+
+    sum() {
+        if (this.length == 0) return 0;
+        else return this.items.reduce((output, ele) => output + ele)
+
+
+    }
+
 };
 
 module.exports = SortedList;
